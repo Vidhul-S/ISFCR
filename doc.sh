b087c44436b86555afc94264f31ad272cf2fc3ac
Vidhul-S <102758360+Vidhul-S@users.noreply.github.com>
2024-09-05 12:15:05 +0530

    Add files via upload

---
+++ doc.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+xhost +local:root
+sudo docker run -it --runtime nvidia \
+  --name test1 \
+  --device /dev/video0 \
+  --device /dev/nvhost-ctrl \
+  --device /dev/nvhost-ctrl-gpu \
+  --device /dev/nvhost-prof-gpu \
+  --device /dev/nvmap \
+  --device /dev/nvhost-gpu \
+  --device /dev/nvhost-vic \
+  --device /dev/nvhost-nvdec \
+  --device /dev/nvhost-msenc \
+  --device /dev/nvhost-isp \
+  --device /dev/nvhost-vi \
+  --volume "/tmp/argus_socket:/tmp/argus_socket" \
+  --volome "/home/intern/UWD/models:/ultralytics/models" \
+  --net="host" \
+  --env="DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --volume="/tmp/.X11-unix:/tmp/.X11unix:rw" \
+  --privileged \
+  ultralytics/ultralytics:latest-jetson-jetpack4 \
+
